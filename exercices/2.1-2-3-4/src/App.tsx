f3c0ce58ef72abf511c3890388054abc54bb21c6
kyllian-delbauche-vinci <kylliandelbauchep@gmail.com>
2024-11-03 11:18:25 +0100

    new:ex2.2

--- exercices/2.1-2-3-4/src/App.tsx
+++ exercices/2.1-2-3-4/src/App.tsx
@@ -1,54 +1,68 @@
+// Définition des types abstraits
+
+type Movie = {
+  title: string;
+  director: string;
+};
+
+type PageTitleProps = {
+  pageTitle: string;
+};
+
+type CinemaProps = {
+  cinemaName: string;
+  movies: Movie[];
+};
+
+// Composent App
 const App = () => {
 
-  // definition des variables
+    // définition des variables, les informations sur les films et les cinémas
 
-  const PageTitle = (props: { title: string }) => {
-    return <h1>{props.title}</h1>;
-  }
-  const Cinema = (props: { name: string, movie1Title: string, movie1Director: string, movie2Title: string, movie2Director: string }) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>{props.movie1Title}</p>
-        <p>{props.movie1Director}</p>
-        <p>{props.movie2Title}</p>
-        <p>{props.movie2Director}</p>
-      </div>
-    );
-  }
+    const pageTitle: PageTitleProps = { pageTitle: "Informations sur les films dans les cinémas" };
 
-  const pageTitle = "Informations sur les films dans les cinémas";
-  const cinema1Name = "UGC DeBrouckère";
-  const cinema1Movie1Title = "Film 1 - DeBrouckère";
-  const cinema1Movie1Director = "Director A";
-  const cinema1Movie2Title = "Film 2 - DeBrouckère";
-  const cinema1Movie2Director = "Director B";
+    const cinema1: CinemaProps = {
+      cinemaName: "UGC DeBrouckère",
+      movies: [
+        { title: "HAIKYU-THE DUMPSTER BATTLE", director: "Susumu Mitsunaka" },
+        { title: "GOODBYE JULIA", director: "Mohamed Kordofani" }
+      ]
+    };
+  
+    const cinema2: CinemaProps = {
+      cinemaName: "UGC Toison d'Or",
+      movies: [
+        { title: "THE WATCHERS", director: "Ishana Night Shyamalan" },
+        { title: "BAD BOYS: RIDE OR DIE", director: "Adil El Arbi, Bilall Fallah" }
+      ]
+    };
 
-  const cinema2Name = "UGC Toison d'Or";
-  const cinema2Movie1Title = "Film 1 - Toison d'Or";
-  const cinema2Movie1Director = "Director C";
-  const cinema2Movie2Title = "Film 2 - Toison d'Or";
-  const cinema2Movie2Director = "Director D";
 
+  // Affichage des informations sur les films et les cinémas
+  
   return (
     <div>
-      <PageTitle title={pageTitle} />
-
-      <Cinema
-        name={cinema1Name}
-        movie1Title={cinema1Movie1Title}
-        movie1Director={cinema1Movie1Director}
-        movie2Title={cinema1Movie2Title}
-        movie2Director={cinema1Movie2Director}
-      />
-
-      <Cinema
-        name={cinema2Name}
-        movie1Title={cinema2Movie1Title}
-        movie1Director={cinema2Movie1Director}
-        movie2Title={cinema2Movie2Title}
-        movie2Director={cinema2Movie2Director}
-      />
+      <h1>{pageTitle.pageTitle}</h1>
+      <div>
+        <h2>{cinema1.cinemaName}</h2>
+        <ul>
+          {cinema1.movies.map((movie, index) => ( // boucle pour afficher les films
+            <li key={index}> {/* // clé pour chaque élément de la liste */}
+              Film {index + 1} : <strong>{movie.title}</strong> - {movie.director} {/* // affichage du titre et du réalisateur */}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>{cinema2.cinemaName}</h2>
+        <ul>
+          {cinema2.movies.map((movie, index) => (
+            <li key={index}>
+              Film {index + 1} : <strong>{movie.title}</strong> - {movie.director}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
